2e120208424b8c1e55ecbe550fa0141792275ecc
Lijiahao <1040374467@qq.com>
2022-06-07 18:01:28 +0800

    6月7日

---
+++ app/src/main/java/com/example/myapplication/broadcast/DemoBroadcastReceiver1.java
@@ -0,0 +1,12 @@
+package com.example.myapplication.broadcast;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class DemoBroadcastReceiver1 extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+    }
+}
