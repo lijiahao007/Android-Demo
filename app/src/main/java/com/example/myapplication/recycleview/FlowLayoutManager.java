d060eaf973c7626a3637e7ae91d5ace86c57e835
李嘉浩 <1040374467@qq.com>
2022-10-31 18:09:20 +0800

    feat: FlowLayoutManager 完善

--- app/src/main/java/com/example/myapplication/recycleview/FlowLayoutManager.java
+++ app/src/main/java/com/example/myapplication/recycleview/FlowLayoutManager.java
@@ -8,6 +8,9 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.macrovideo.sdk.tools.LogUtils;
 
+import java.util.ArrayList;
+import java.util.List;
+
 // TODO: 回收
 
 public class FlowLayoutManager extends RecyclerView.LayoutManager {
@@ -110,8 +113,6 @@ public class FlowLayoutManager extends RecyclerView.LayoutManager {
         // dy > 0 --> 手指往上滑， 需要填充下方元素
         // dy < 0 --> 手指往下滑， 需要填充上方的元素
 
-//        Log.i("FlowLayoutManager", "dy=" + dy);
-
         // 1. 填充子View
         fill(dy, recycler);
 
@@ -121,7 +122,6 @@ public class FlowLayoutManager extends RecyclerView.LayoutManager {
         // 3. 回收子View
         recycler(dy, recycler);
 
-//        Log.i(TAG, "scrapList.size=" + recycler.getScrapList().size() + " childCount=" + getChildCount());
 
         return dy;
     }
@@ -148,6 +148,9 @@ public class FlowLayoutManager extends RecyclerView.LayoutManager {
                 int fillPosition = adapterPosition + 1; // 被填充的Item在Adapter中的位置。
 
                 // 2. addView
+                if (fillPosition >= getItemCount()) {
+                    break;
+                }
                 View viewForPosition = recycler.getViewForPosition(fillPosition);
                 addView(viewForPosition);
                 RecyclerView.LayoutParams insertViewParam = (RecyclerView.LayoutParams) viewForPosition.getLayoutParams();
@@ -188,17 +191,100 @@ public class FlowLayoutManager extends RecyclerView.LayoutManager {
             // 填充头部元素
             while (true) {
                 // 1. 找到锚点元素
-                View childAt = getChildAt(0);
-                int firstPosition = getPosition(childAt);
-                int fillPosition = firstPosition - 1;
+                View anchorView = getChildAt(0);
+                int anchorPosition = getPosition(anchorView);
+                RecyclerView.LayoutParams layoutParams = (RecyclerView.LayoutParams) anchorView.getLayoutParams();
+                int anchorTopMargin = layoutParams.topMargin;
+                int decoratedTop = getDecoratedTop(anchorView);
+                int fillPosition = anchorPosition - 1;
 
-                // 查看锚点元素那一行还有没有空位，如果有，则插入到那一行的第一个。如果没有则插入到上一行的第一个
+                // 2. 从Recycler获取目标View,
+                if (fillPosition < 0) {
+                    break;
+                }
+                View viewForPosition = recycler.getViewForPosition(fillPosition);
+                RecyclerView.LayoutParams targetViewParam = (RecyclerView.LayoutParams) viewForPosition.getLayoutParams();
+                int leftMargin = targetViewParam.leftMargin;
+                int rightMargin = targetViewParam.rightMargin;
+                int bottomMargin = targetViewParam.bottomMargin;
+
+                // 3. 测量View的大小
+                addView(viewForPosition, 0);
+                measureChildWithMargins(viewForPosition, 0, 0);
+                int height = getDecoratedMeasuredHeight(viewForPosition);
+                int width = getDecoratedMeasuredWidth(viewForPosition);
 
 
+                // 4. 查看锚点元素那一行还有没有空位，如果有，则插入到那一行的第一个。如果没有则插入到上一行的第一个
+                ArrayList<View> currentLineViews = getCurrentLineView(anchorView);
+                Log.i(TAG, currentLineViews.toString());
+                View lastView = currentLineViews.get(currentLineViews.size() - 1);
+                int rightestInLine = getDecoratedRight(lastView);
 
+                if (rightestInLine + width + leftMargin <= getWidth()) {
+                    // 插入当前行的第一个。
+                    layoutDecorated(viewForPosition,
+                            leftMargin,
+                            decoratedTop,
+                            leftMargin + width,
+                            decoratedTop + height);
 
+                    int lineStart = leftMargin + width + rightMargin;
+                    for (int i = 0; i < currentLineViews.size(); i++) {
+                        View curLine = currentLineViews.get(i);
+                        int position = getPosition(curLine);
+                        removeAndRecycleView(curLine, recycler);
+                        View view = recycler.getViewForPosition(position);
+                        addView(view, i + 1);
+                        RecyclerView.LayoutParams layoutParam = (RecyclerView.LayoutParams) view.getLayoutParams();
+                        measureChildWithMargins(view, 0, 0);
+                        int viewHeight = getDecoratedMeasuredHeight(view);
+                        int viewWidth = getDecoratedMeasuredWidth(view);
+
+                        layoutDecorated(view,
+                                lineStart + layoutParam.leftMargin,
+                                decoratedTop,
+                                lineStart + layoutParam.leftMargin + viewWidth,
+                                decoratedTop + viewHeight
+                        );
+
+                        lineStart += layoutParam.leftMargin + viewWidth + layoutParam.rightMargin;
+                    }
+                } else {
+                    // 插入上一行的第一个
+
+                    // 如果超出范围，则不添加
+                    if (decoratedTop - anchorTopMargin - bottomMargin < 0) {
+                        removeAndRecycleView(viewForPosition, recycler);
+                        break;
+                    }
+
+                    layoutDecorated(viewForPosition,
+                            leftMargin,
+                            decoratedTop - anchorTopMargin - bottomMargin - height,
+                            leftMargin + width,
+                            decoratedTop - anchorTopMargin - bottomMargin
+                    );
+                }
             }
         }
+    }
+
+    /**
+     * @param anchorView 返回与anchorView同一行的View
+     * @return
+     */
+    private ArrayList<View> getCurrentLineView(View anchorView) {
+        int anchorTop = getDecoratedTop(anchorView);
+        ArrayList<View> res = new ArrayList<>();
+        for (int i = 0; i < getChildCount(); i++) {
+            View childAt = getChildAt(i);
+            int decoratedTop = getDecoratedTop(childAt);
+            if (decoratedTop == anchorTop) {
+                res.add(childAt);
+            }
+        }
+        return res;
     }
 
     /**
