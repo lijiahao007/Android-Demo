dcbcd577bc21b80f0c81eca3db03d56cab936dbc
李嘉浩 <1040374467@qq.com>
2022-11-28 00:42:39 +0800

    软键盘相关

--- app/src/main/java/com/example/myapplication/statusinset/SoftInputState2Activity.java
+++ app/src/main/java/com/example/myapplication/statusinset/SoftInputState2Activity.java
@@ -1,14 +1,43 @@
 package com.example.myapplication.statusinset;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 
 import com.example.myapplication.BaseActivity;
+import com.example.myapplication.R;
 import com.example.myapplication.databinding.ActivitySoftInputState2Binding;
 
 public class SoftInputState2Activity extends BaseActivity<ActivitySoftInputState2Binding> {
 
     @Override
+    protected int[] bindClick() {
+        return new int[]{R.id.btn_back};
+    }
+
+    @Override
+    public void onClick(View v) {
+        super.onClick(v);
+        switch (v.getId()) {
+            case R.id.btn_back:
+                this.finish();
+                break;
+        }
+    }
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Window window = getWindow();
+        WindowManager.LayoutParams attributes = window.getAttributes();
+
+        // 1. state_unspecified : 是softInputMode的默认值，
+        attributes.softInputMode = WindowManager.LayoutParams.SOFT_INPUT_STATE_UNCHANGED;
+        window.setAttributes(attributes);
+//
+//        binding.editText.setFocusable(true);
+//        binding.editText.setFocusableInTouchMode(true);
+//        binding.editText.requestFocus();
     }
 }
--- app/src/main/java/com/example/myapplication/statusinset/SoftInputStateActivity.java
+++ app/src/main/java/com/example/myapplication/statusinset/SoftInputStateActivity.java
@@ -1,16 +1,40 @@
 package com.example.myapplication.statusinset;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 
 import com.example.myapplication.BaseActivity;
+import com.example.myapplication.R;
 import com.example.myapplication.databinding.ActivitySoftInputStateBinding;
 
 public class SoftInputStateActivity extends BaseActivity<ActivitySoftInputStateBinding> {
 
+    @Override
+    protected int[] bindClick() {
+        return new int[] { R.id.btn_launch_activity_next};
+    }
+
+    @Override
+    public void onClick(View v) {
+        super.onClick(v);
+        switch (v.getId()) {
+            case R.id.btn_launch_activity_next:
+                startActivity(new Intent(this, SoftInputState2Activity.class));
+                break;
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Window window = getWindow();
+        WindowManager.LayoutParams attributes = window.getAttributes();
+
+        // 1. state_unspecified : 是softInputMode的默认值，
+        attributes.softInputMode = WindowManager.LayoutParams.SOFT_INPUT_STATE_UNCHANGED;
+        window.setAttributes(attributes);
     }
 }
